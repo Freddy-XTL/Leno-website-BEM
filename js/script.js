49293ace8b2773c46ddc4cb20478c937cc43209c
Freddy-XTL <zacklightningxyz@gmail.com>
2026-09-24 14:09:58 +0200

    Added preview section with video modal

--- js/script.js
+++ js/script.js
@@ -5,6 +5,30 @@ document.addEventListener("DOMContentLoaded", function() {
     toggleButton.addEventListener("click", function() {
         mobileMenu.classList.toggle("active");
     });
+
+    // Video Modal
+    const modal = document.getElementById("videoModal");
+    const videoButton = document.querySelector(".preview__video-button");
+    const closeButton = document.querySelector(".modal__close-button");
+    const videoPlayer = document.getElementById("videoPlayer");
+
+    videoButton.addEventListener("click", function() {
+        modal.style.display = "block";
+
+        videoPlayer.src = "https://youtu.be/yTyi4ZBLC_I?t=55"
+
+        closeButton.addEventListener("click", function() {
+            modal.style.display = "none";
+            videoPlayer.src = "";
+        });
+
+        window.addEventListener("click", function() {
+            if(event.target == modal) {
+                modal.style.display = "none";
+                videoPlayer.src = "";
+            }
+        })
+    })
 });
 
 window.addEventListener("scroll", function() {
